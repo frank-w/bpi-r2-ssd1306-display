a920f5115bd1b48060bfd70983911a69b621f60c
Frank Wunderlich <frank-w@public-files.de>
2021-03-08 19:24:15 +0100

    stats: fix missing path

--- stats.py
+++ stats.py
@@ -46,7 +46,7 @@ if use_img:
     from PIL import ImageDraw
     from PIL import ImageFont
 
-iconpath = os.path.dirname(__file__)
+iconpath = os.path.dirname(os.path.abspath(__file__))
 iconwidth = 16
 
 ###################################################################################
